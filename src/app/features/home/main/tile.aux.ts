ba46d47cc2f61f1ef9f55b70caa83522a3c6ec81
aamanh30 <aamanh30@gmail.com>
2024-02-11 18:02:44 +0530

    feat(home) - main.component and tile.aux - Updated the config to add the link property and updated the template to show the cards are clickable links

--- src/app/features/home/main/tile.aux.ts
+++ src/app/features/home/main/tile.aux.ts
@@ -1,20 +1,24 @@
-import { Tile } from '../../../shared/models';
+import { TileLink } from '../../../shared/models';
 
-export const tiles: ReadonlyArray<Tile> = [
+export const tiles: ReadonlyArray<TileLink> = [
   {
     name: 'Veg',
     image: 'assets/icons/vegetarian.png',
+    link: 'vegetarian',
   },
   {
     name: 'Non Veg',
     image: 'assets/icons/non-vegetarian.png',
+    link: 'non-vegetarian',
   },
   {
     name: 'Kosher',
     image: 'assets/icons/kosher.png',
+    link: 'kosher',
   },
   {
     name: 'Chinese',
     image: 'assets/icons/chinese.png',
+    link: 'chinese',
   },
 ];
