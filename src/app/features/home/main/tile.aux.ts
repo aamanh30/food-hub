50cb4c5a0a80c5201c49636ddb94b28ed925fc99
aamanh30 <aamanh30@gmail.com>
2024-02-18 02:54:18 +0530

    refactor(hom) - tile.aux.ts - Updated the links of the tiles in config with proper links

--- src/app/features/home/main/tile.aux.ts
+++ src/app/features/home/main/tile.aux.ts
@@ -4,21 +4,21 @@ export const tiles: ReadonlyArray<TileLink> = [
   {
     name: 'Veg',
     image: 'assets/icons/vegetarian.png',
-    link: 'vegetarian',
+    link: '/catalogue/vegetarian'
   },
   {
     name: 'Non Veg',
     image: 'assets/icons/non-vegetarian.png',
-    link: 'non-vegetarian',
+    link: '/catalogue/non-vegetarian'
   },
   {
     name: 'Kosher',
     image: 'assets/icons/kosher.png',
-    link: 'kosher',
+    link: '/catalogue/kosher'
   },
   {
     name: 'Chinese',
     image: 'assets/icons/chinese.png',
-    link: 'chinese',
-  },
+    link: '/catalogue/chinese'
+  }
 ];
